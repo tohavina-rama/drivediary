2a70097f92af77fea200dcb7fbf7a2d4e34917c3
Tohavina RAMANANJATOVO <tohavinarama@gmail.com>
2025-05-27 11:22:52 +0200

    Add HTML structure for dashboard and goal pages; update header with navigation links

--- includes/header.php
+++ includes/header.php
@@ -1,18 +1,10 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <title>Dashboard</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-
-<body>
-    <header>
-        <ul>
-            <li><a href="index.php">Accueil</a></li>
-            <li><a href="index.php">Accueil</a></li>
-            <li><a href="index.php">Accueil</a></li>
-           
-            </ul>
-    </header>
+<header>
+    <ul>
+        <li><a href="dashboard.php">Dashboard</a></li>
+        <li><a href="goal.php">Objectif</a></li>
+       
+    </ul>
+    <form action="logout.php" method="post" style="display:inline;">
+        <button type="submit">Déconnexion</button>
+    </form>
+</header>
