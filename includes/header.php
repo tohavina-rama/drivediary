a7797156269afd769f2809416825773511c70241
Tohavina RAMANANJATOVO <tohavinarama@gmail.com>
2025-05-27 11:13:26 +0200

    Add initial HTML structure for dashboard, goal, header, and index pages

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <header>
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="index.php">Accueil</a></li>
+           
+            </ul>
+    </header>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Drivediary</title>
+</head>
+<body>
+    <form method="post" action="">
+        <label for="nom">Nom :</label>
+        <input type="text" id="nom" name="nom" required>
+        <button type="submit">Valider</button>
+    </form>
+</body>
+</html>
